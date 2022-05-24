8586e14e9b04e96e5a364f46f7b376438a980d63
davit03 <106078777+davit03@users.noreply.github.com>
2022-05-24 05:30:15 -0700

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello!!!");
-        System.out.pritnln("Hello again");
+        System.out.println("Hello again");
     }
 }
